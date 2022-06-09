eb5ceabc921e33257ee8ad69a9bbdb130952d207
Jesus Barros <jesus.reinaldo.barros@gmail.com>
2022-06-09 11:53:01 -0500

    Update

---
+++ src/builder/componentes.js
@@ -0,0 +1,15 @@
+// Clase Principal
+class UnidadCentral {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+// Componentes (varios elementos perifericos)
+
+// Clase Padre
+class Componentes {
+  constructor(name) {
+    this.name = name;
+  }
+}
